a0c464dde1a10b95f36a0429c7082ff21a14587b
Gulyapas Poonkawinsiri <gulyapas.p@gmail.com>
2022-06-13 20:39:51 +0700

    add custom component and css module

---
+++ pages/[meetupId]/index.js
@@ -0,0 +1,14 @@
+import MeetupDetail from "../../components/meetups/MeetupDetail";
+
+const MeetupDetails = () => {
+  return (
+    <MeetupDetail
+      image="https://s3-us-west-2.amazonaws.com/cdn.panda-gossips.com/production/posts/eyecatches/000/005/789/original.jpg?1581084857"
+      title="First Meetup"
+      address="Some Street 5, Some City"
+      description="The meetup description"
+    />
+  );
+};
+
+export default MeetupDetails;
