48a5563a40980dc4fa4a6daaacf5cc5e2d763431
Gulyapas Poonkawinsiri <gulyapas.p@gmail.com>
2022-06-13 20:02:37 +0700

    adding the new-meetup form

--- pages/new-meetup/index.js
+++ pages/new-meetup/index.js
@@ -1,1 +1,12 @@
-    
+import React from "react";
+import NewMeetupForm from "../../components/meetups/NewMeetupForm";
+
+const NewMeetupPage = () => {
+  const addMeetupHandler = (enteredMeetupData) => {
+      console.log(enteredMeetupData)
+  };
+
+  return <NewMeetupForm onAddMeetup={addMeetupHandler} />;
+};
+
+export default NewMeetupPage;
