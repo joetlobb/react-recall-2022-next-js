9891426645894079d3b326d8cf582b2f48e20c22
Gulyapas Poonkawinsiri <gulyapas.p@gmail.com>
2022-06-15 04:25:24 +0700

    working with SSG and params to generate dynamic pages, dynamic supported paths need to be prepared with getStaticPaths() and using of fallback pages

--- pages/[meetupId]/index.js
+++ pages/[meetupId]/index.js
@@ -11,4 +11,31 @@ const MeetupDetails = () => {
   );
 };
 
+export const getStaticPaths = async () => {
+  return {
+    // if fallback true: others page not supported will be generated from the server
+    // if fallback false: it means that we have declared all support paths
+    fallback: false,
+    paths: [{ params: { meetupId: "m1" } }, { params: { meetupId: "m2" } }],
+  };
+};
+
+export const getStaticProps = async (context) => {
+  const meetupId = context.params.meetupId;
+  console.log(meetupId);
+
+  return {
+    props: {
+      meetupData: {
+        image:
+          "https://s3-us-west-2.amazonaws.com/cdn.panda-gossips.com/production/posts/eyecatches/000/005/789/original.jpg?1581084857",
+        id: "m1",
+        title: "First Meetup",
+        address: "Some Street 5, Some City",
+        description: "The meetup description",
+      },
+    },
+  };
+};
+
 export default MeetupDetails;
--- pages/index.js
+++ pages/index.js
@@ -24,33 +24,33 @@ const HomePage = (props) => {
   return <MeetupList meetups={props.meetups} />;
 };
 
-// // this is Static Generation, async is allow, nextjs will look for this func
-// // with this reserved name to fetch data before return this component
-// // only allowed in pages component (inside pages folder)
-// // client will not see but the server will provide since production build process
-// export const getStaticProps = async () => {
-//   // fetch data from a server
-//   return {
-//     props: {
-//       meetups: DUMMY_MEETUPS,
-//     },
-//     // this property will regenerate this page in seconds,
-//     // so this page will never be older than 1 second
-//     revalidate: 1,
-//   };
-// };
-
-// another approach is Server Side Rendering SSR, this will generate the page
-// in every request, it also provide request and respond for authentication
-// it is a disadvantage if we do not require such feature because it will be
-// slower than SSG
-export const getServerSideProps = async (context) => {
-  const req = context.req;
-  const res = context.res;
-  // fetch data from server
+// this is Static Generation, async is allow, nextjs will look for this func
+// with this reserved name to fetch data before return this component
+// only allowed in pages component (inside pages folder)
+// client will not see but the server will provide since production build process
+export const getStaticProps = async () => {
+  // fetch data from a server
   return {
-    props: { meetups: DUMMY_MEETUPS },
+    props: {
+      meetups: DUMMY_MEETUPS,
+    },
+    // this property will regenerate this page in seconds,
+    // so this page will never be older than 1 second
+    revalidate: 1,
   };
 };
+
+// // another approach is Server Side Rendering SSR, this will generate the page
+// // in every request, it also provide request and respond for authentication
+// // it is a disadvantage if we do not require such feature because it will be
+// // slower than SSG
+// export const getServerSideProps = async (context) => {
+//   const req = context.req;
+//   const res = context.res;
+//   // fetch data from server
+//   return {
+//     props: { meetups: DUMMY_MEETUPS },
+//   };
+// };
 
 export default HomePage;
