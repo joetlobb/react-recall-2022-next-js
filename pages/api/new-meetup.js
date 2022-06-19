dddc8d3f1fe6172135d36b638fd963431b8a3b06
Gulyapas Poonkawinsiri <gulyapas.p@gmail.com>
2022-06-19 20:48:51 +0700

    fix user and password import for mongoDB database, sending http request to our API route, add POST logic to the new-meetup page and using router to push to homepage

--- pages/api/new-meetup.js
+++ pages/api/new-meetup.js
@@ -1,26 +1,26 @@
-// /api/new-meetup
 import { MongoClient } from "mongodb";
-import mongodbPass from "./mongodb";
+import { mongoUser, mongoPassword } from "./mongodb";
+
+// /api/new-meetup
+// POST /api/new-meetup
 
 async function handler(req, res) {
   if (req.method === "POST") {
     const data = req.body;
-    // const { title, image, address, description } = data;
 
     const client = await MongoClient.connect(
-      "mongodb+srv://Gulyapasp:" +
-        mongodbPass +
-        "@cluster0.gdiyk.mongodb.net/meetups?retryWrites=true&w=majority"
+      `mongodb+srv://${mongoUser}:${mongoPassword}@cluster0.gdiyk.mongodb.net/meetups?retryWrites=true&w=majority`
     );
     const db = client.db();
 
     const meetupsCollection = db.collection("meetups");
+
     const result = await meetupsCollection.insertOne(data);
 
     console.log(result);
 
     client.close();
-    res.status(201).json({ message: "Meetup Inserted!" }); // 201 something insert successfully
+    res.status(201).json({ message: "Meetup inserted!" }); // 201 something insert successfully
   }
 }
 
